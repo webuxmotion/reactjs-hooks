3e00a17b99c48c894ade0eacfed0878b28dac689
Andrii Pereverziev <a.pereverziev@mail.enamine.net>
2021-06-02 18:59:06 +0300

    Initial commit. useState, useEffect, useRef

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import EffectExample from "./EffectExample";
+import StateExample from './StateExample';
+import RefExample from './RefExample';
+
+function App() {
+  
+
+  return (
+    <div>
+      <div>APP</div>
+      {/* <EffectExample /> */}
+      {/* <StateExample /> */}
+      <RefExample />
+    </div>
+  );
+}
+
+export default App;
